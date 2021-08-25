8c5151db71136defe2230c512426f16c4fb1920c
OwOBox <89490108+OwOBox@users.noreply.github.com>
2021-08-25 18:06:10 +0700

    Update ytsearch.py

--- StreamMusic/modules/ytsearch.py
+++ StreamMusic/modules/ytsearch.py
@@ -36,7 +36,7 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 async def ytsearch(_, message: Message):
     try:
         if len(message.command) < 2:
-            await message.reply_text("/search needs an argument!")
+            await message.reply_text("/search tên bài hát")
             return
         query = message.text.split(None, 1)[1]
         m = await message.reply_text("Searching....")
@@ -44,10 +44,10 @@ async def ytsearch(_, message: Message):
         i = 0
         text = ""
         while i < 4:
-            text += f"Title - {results[i]['title']}\n"
-            text += f"Duration - {results[i]['duration']}\n"
-            text += f"Views - {results[i]['views']}\n"
-            text += f"Channel - {results[i]['channel']}\n"
+            text += f"{results[i]['title']}\n"
+            text += f"Thời lượng: {results[i]['duration']}\n"
+ #           text += f"Views - {results[i]['views']}\n"
+ #           text += f"Channel - {results[i]['channel']}\n"
             text += f"https://youtube.com{results[i]['url_suffix']}\n\n"
             i += 1
         await m.edit(text, disable_web_page_preview=True)
